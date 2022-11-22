9cf91d0a37184517c697f9600dceede62b7cdcb6
Elena Poelman <elena.poelman@redpencil.io>
2022-11-22 17:42:05 +0100

    add import-snippet plugin

--- test-app/app/controllers/application.js
+++ test-app/app/controllers/application.js
@@ -1,17 +1,26 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
+  @service importRdfaSnippet;
   plugins = [
     'besluit-type',
     'citaten',
+    'import-snippet',
     'rdfa-date',
     'standard-template',
     'table-of-contents',
   ];
 
   @action
-  rdfaEditorInit(controller) {
+  async rdfaEditorInit(controller) {
+    await this.importRdfaSnippet.downloadSnippet({
+      omitCredentials: 'true',
+      source:
+        'https://dev.kleinbord.lblod.info/snippets/example-opstellingen.html',
+      mock: 'true',
+    });
     const editorDone = new CustomEvent('editor-done');
     window.dispatchEvent(editorDone);
   }
