afc9cc74bb9e086f5f577d10d7b716f091ccd35d
Elena Poelman <elena.poelman@redpencil.io>
2022-11-23 16:30:26 +0100

    add insert and template variable plugins

---
+++ ember-rdfa-editor-lblod-plugins/src/utils/variable-plugins/fetch-data.js
@@ -0,0 +1,98 @@
+function generateCodeListOptionsQuery(codelistUri) {
+  const codeListOptionsQuery = `
+    PREFIX lblodMobilitiet: <http://data.lblod.info/vocabularies/mobiliteit/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX schema: <http://schema.org/>
+    SELECT DISTINCT * WHERE { 
+      <${codelistUri}> a lblodMobilitiet:Codelist.
+      ?codelistOptions skos:inScheme <${codelistUri}>.
+      ?codelistOptions skos:prefLabel ?label.
+      OPTIONAL {
+        ?codelistOptions schema:position ?position .
+      }
+      OPTIONAL {
+        <${codelistUri}> dct:type ?type.
+      }
+    }
+    ORDER BY (!BOUND(?position)) ASC(?position)
+  `;
+  return codeListOptionsQuery;
+}
+
+function generateCodeListsByPublisherQuery(publisher) {
+  const codeListOptionsQuery = `
+    PREFIX lblodMobilitiet: <http://data.lblod.info/vocabularies/mobiliteit/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    SELECT DISTINCT * WHERE { 
+      ?uri a lblodMobilitiet:Codelist;
+        skos:prefLabel ?label.
+      ${
+        publisher
+          ? `
+        ?uri dct:publisher <${publisher}>.
+      `
+          : ''
+      }
+    }
+  `;
+  return codeListOptionsQuery;
+}
+
+export async function fetchCodeListOptions(endpoint, codelistUri) {
+  const codelistsOptionsQueryResult = await executeQuery(
+    endpoint,
+    generateCodeListOptionsQuery(codelistUri)
+  );
+  const options = parseCodelistOptions(codelistsOptionsQueryResult);
+  return {
+    type:
+      codelistsOptionsQueryResult.results.bindings[0] &&
+      codelistsOptionsQueryResult.results.bindings[0].type
+        ? codelistsOptionsQueryResult.results.bindings[0].type.value
+        : '',
+    options,
+  };
+}
+
+export async function fetchCodeListsByPublisher(endpoint, publisher) {
+  const codelistsOptionsQueryResult = await executeQuery(
+    endpoint,
+    generateCodeListsByPublisherQuery(publisher)
+  );
+  const bindings = codelistsOptionsQueryResult.results.bindings;
+  return bindings.map((binding) => ({
+    uri: binding.uri.value,
+    label: binding.label.value,
+  }));
+}
+
+function parseCodelistOptions(queryResult) {
+  const bindings = queryResult.results.bindings;
+  return bindings.map((binding) => ({
+    value: binding.label.value,
+    label: binding.label.value,
+  }));
+}
+
+async function executeQuery(endpoint, query) {
+  const encodedQuery = encodeURIComponent(query.trim());
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/sparql-results+json',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    },
+    body: `query=${encodedQuery}`,
+  });
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error(
+      `Request to MOW backend was unsuccessful: [${response.status}] ${response.statusText}`
+    );
+  }
+}
--- test-app/app/controllers/application.js
+++ test-app/app/controllers/application.js
@@ -4,14 +4,75 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
   @service importRdfaSnippet;
+  prefixes = {
+    ext: 'http://mu.semte.ch/vocabularies/ext/',
+    mobiliteit: 'https://data.vlaanderen.be/ns/mobiliteit#',
+    dct: 'http://purl.org/dc/terms/',
+  };
+
   plugins = [
     'besluit-type',
     'citaten',
     'import-snippet',
+    {
+      name: 'insert-variable',
+      options: {
+        defaultEndpoint: 'https://dev.roadsigns.lblod.info/sparql',
+        variableTypes: [
+          'text',
+          'number',
+          'date',
+          'codelist',
+          {
+            label: 'Dummy Variable',
+            fetchSubtypes: async () => {
+              const codelists = [
+                {
+                  uri: '1',
+                  label: '1',
+                },
+                {
+                  uri: '2',
+                  label: '2',
+                },
+                {
+                  uri: '3',
+                  label: '3',
+                },
+              ];
+              return codelists;
+            },
+            template: (endpoint, selectedCodelist) => `
+              <span property="ext:codelist" resource="${selectedCodelist.uri}"></span>
+              <span property="dct:type" content="location"></span>
+              <span property="dct:source" resource="${endpoint}"></span>
+              <span property="ext:content" datatype="xsd:date">
+                <span class="mark-highlight-manual">\${${selectedCodelist.label}}</span>
+              </span>
+            `,
+          },
+        ],
+      },
+    },
     'rdfa-date',
     'standard-template',
     'table-of-contents',
+    'template-variable',
   ];
+
+  @action
+  setPrefixes(element) {
+    element.setAttribute('prefix', this.prefixToAttrString(this.prefixes));
+  }
+
+  prefixToAttrString(prefix) {
+    let attrString = '';
+    Object.keys(prefix).forEach((key) => {
+      let uri = prefix[key];
+      attrString += `${key}: ${uri} `;
+    });
+    return attrString;
+  }
 
   @action
   async rdfaEditorInit() {
