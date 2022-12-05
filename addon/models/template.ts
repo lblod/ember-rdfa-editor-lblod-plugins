b05a8422f7f7ea52861cff9196f8c59b5081bbae
Elena Poelman <elena.poelman@redpencil.io>
2022-12-05 09:25:10 +0100

    add string-set transform types

--- addon/models/template.ts
+++ addon/models/template.ts
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import Model, { attr } from '@ember-data/model';
-
 declare module 'ember-data/types/registries/model' {
   export default interface ModelRegistry {
     template: TemplateModel;
@@ -8,12 +6,9 @@ declare module 'ember-data/types/registries/model' {
 }
 export default class TemplateModel extends Model {
   @attr declare title: string;
-  // @ts-ignore
   @attr('string-set', { defaultValue: () => [] }) declare matches: string[];
   @attr declare body: string;
-  // @ts-ignore
   @attr('string-set', { defaultValue: () => [] }) declare contexts: string[];
-  // @ts-ignore
   @attr('string-set', { defaultValue: () => [] })
   declare disabledInContexts: string[];
 }
