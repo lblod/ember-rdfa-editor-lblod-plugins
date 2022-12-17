94ac9c16636030b020bd87da034beb6a59c6ebe1
Elena Poelman <elena.poelman@redpencil.io>
2022-12-17 14:21:54 +0100

    improve insert-structure naming and select new article after insertion

--- addon/commands/article-structure-plugin/insert-article-structure.ts
+++ addon/commands/article-structure-plugin/insert-article-structure.ts
@@ -1,6 +1,9 @@
 import { v4 as uuid } from 'uuid';
-import process from 'process';
-import { PNode, ProseController } from '@lblod/ember-rdfa-editor';
+import {
+  PNode,
+  ProseController,
+  TextSelection,
+} from '@lblod/ember-rdfa-editor';
 import IntlService from 'ember-intl/services/intl';
 import { Command } from 'prosemirror-state';
 import { unwrap } from '@lblod/ember-rdfa-editor/utils/option';
@@ -18,13 +21,14 @@ export default function insertArticleStructureV2(
   intlService: IntlService
 ): Command {
   return (_state, dispatch) => {
-    const structureToAddIndex = options.structures.findIndex(
+    const structureTypeToAdd = options.structures.find(
       (structure) => structure.title === structureName
     );
-    const structureToAdd = unwrap(options.structures[structureToAddIndex]);
-    const structureUri = `${structureToAdd.uriBase}${uuid()}`;
-
-    window.process = process;
+    if (!structureTypeToAdd) {
+      console.warn(`Structure type ${structureName} not recognized`);
+      return false;
+    }
+    const structureUri = `${structureTypeToAdd.uriBase}${uuid()}`;
     const urisNotAllowedToInsert = report.results.map(
       (result) => result.focusNode?.value
     );
@@ -36,82 +40,80 @@ export default function insertArticleStructureV2(
       }
       return false;
     };
-    let resourceToInsert = nodesBetween(
-      selection.$from,
-      true,
-      false,
-      filterFunction
-    ).next().value;
-    if (!resourceToInsert) {
-      resourceToInsert = nodesBetween(
-        selection.$from,
-        true,
-        true,
-        filterFunction
-      ).next().value;
-    }
-    if (!resourceToInsert) return false;
-    let nodeToInsert: { node: PNode; pos: number } | undefined | null;
-    if (structureToAdd.insertPredicate) {
-      const resourceToInsertUri = resourceToInsert.node.attrs[
+
+    let structureContainer =
+      nodesBetween(selection.$from, true, false, filterFunction).next().value ??
+      nodesBetween(selection.$from, true, true, filterFunction).next().value;
+
+    if (!structureContainer) return false;
+
+    if (structureTypeToAdd.insertPredicate) {
+      const structureContainerUri = structureContainer.node.attrs[
         'resource'
       ] as string;
-      nodeToInsert = [
+      structureContainer = [
         ...controller.datastore
           .match(
-            `>${resourceToInsertUri}`,
-            `>${structureToAdd.insertPredicate.long}`
+            `>${structureContainerUri}`,
+            `>${structureTypeToAdd.insertPredicate.long}`
           )
           .asPredicateNodeMapping()
           .nodes(),
       ][0];
-    } else {
-      nodeToInsert = resourceToInsert;
     }
-    if (!nodeToInsert) {
+    if (!structureContainer) {
       return false;
     }
     if (dispatch) {
-      const structureHtml = structureToAdd.template(structureUri, intlService);
+      const structureHtml = structureTypeToAdd.template(
+        structureUri,
+        intlService
+      );
 
-      //Detect if nodeToInsert only contains a placeholder, if so replace the full content of the nodeToInsert
+      //Detect if structureContainer only contains a placeholder, if so replace the full content of the structureContainer
+      let insertRange: { from: number; to: number };
       if (
-        nodeToInsert.node.childCount === 1 &&
-        nodeToInsert.node.child(0).childCount === 1 &&
-        nodeToInsert.node.child(0).child(0).type ===
+        structureContainer.node.childCount === 1 &&
+        structureContainer.node.child(0).childCount === 1 &&
+        structureContainer.node.child(0).child(0).type ===
           controller.schema.nodes['placeholder']
       ) {
-        controller.doCommand(
-          insertHtml(
-            structureHtml,
-            nodeToInsert.pos + 1,
-            nodeToInsert.pos + nodeToInsert.node.nodeSize - 1
-          )
-        );
+        insertRange = {
+          from: structureContainer.pos + 1,
+          to: structureContainer.pos + structureContainer.node.nodeSize - 1,
+        };
       } else {
-        controller.doCommand(
-          insertHtml(
-            structureHtml,
-            nodeToInsert.pos + nodeToInsert.node.nodeSize - 1,
-            nodeToInsert.pos + nodeToInsert.node.nodeSize - 1
-          )
-        );
+        insertRange = {
+          from: structureContainer.pos + structureContainer.node.nodeSize - 1,
+          to: structureContainer.pos + structureContainer.node.nodeSize - 1,
+        };
       }
+      controller.doCommand(
+        insertHtml(structureHtml, insertRange.from, insertRange.to)
+      );
       const containerNode = unwrap(
-        controller.state.doc.nodeAt(nodeToInsert.pos)
+        controller.state.doc.nodeAt(structureContainer.pos)
       );
       const containerRange = {
-        from: nodeToInsert.pos,
-        to: nodeToInsert.pos + containerNode.nodeSize,
+        from: structureContainer.pos,
+        to: structureContainer.pos + containerNode.nodeSize,
       };
       controller.doCommand(
         recalculateStructureNumbers(
           controller,
           containerRange,
-          structureToAdd,
+          structureTypeToAdd,
           options
         )
       );
+
+      const selection = TextSelection.near(
+        controller.state.doc.resolve(insertRange.from)
+      );
+      controller.withTransaction((tr) => {
+        return tr.setSelection(selection);
+      });
+      controller.focus();
     }
     return true;
   };
