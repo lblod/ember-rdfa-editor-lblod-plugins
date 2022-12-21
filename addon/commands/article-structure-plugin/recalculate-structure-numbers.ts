18d3239b1b5a2685ae075059c67d7077be384ab6
Elena Poelman <elena.poelman@redpencil.io>
2022-12-21 12:52:15 +0100

    article structure plugin: refactor recalculate-structure-numbers a bit

--- addon/commands/article-structure-plugin/recalculate-structure-numbers.ts
+++ addon/commands/article-structure-plugin/recalculate-structure-numbers.ts
@@ -2,12 +2,11 @@ import { ProseController } from '@lblod/ember-rdfa-editor';
 import defaults from '@lblod/ember-rdfa-editor-lblod-plugins/utils/article-structure-plugin/defaults';
 import { Command } from 'prosemirror-state';
 import { ProseStore } from '@lblod/ember-rdfa-editor/utils/datastore/prose-store';
-import { expect, unwrap } from '@lblod/ember-rdfa-editor/utils/option';
+import { expect } from '@lblod/ember-rdfa-editor/utils/option';
 import {
   ResolvedArticleStructurePluginOptions,
   StructureSpec,
 } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/article-structure-plugin';
-import { getRdfaAttribute } from '@lblod/ember-rdfa-editor/utils/rdfa-utils';
 
 export default function recalculateStructureNumbers(
   controller: ProseController,
@@ -22,12 +21,13 @@ export default function recalculateStructureNumbers(
         datastore = controller.datastore.limitToRange(
           controller.state,
           containerRange.from,
-          containerRange.to
+          containerRange.to,
+          'rangeContains'
         );
       } else {
         datastore = controller.datastore;
       }
-      const structures = [
+      const structureMatch = [
         ...datastore
           .match(null, 'a', `>${structureType.type}`)
           .transformDataset((dataset) => {
@@ -35,17 +35,17 @@ export default function recalculateStructureNumbers(
               return options.structureTypes.includes(quad.object.value);
             });
           })
-          .asPredicateNodeMapping()
-          .nodes(),
+          .asSubjectNodeMapping(),
       ];
-      if (!structures) return true;
-      // Temporary workaround: for some reason structures contains each structure twice.
-      const structuresFiltered = structures.filter(
-        (node, index) => index === structures.indexOf(node)
-      );
-      for (let i = 0; i < structuresFiltered.length; i++) {
-        const structure = unwrap(structuresFiltered[i]);
-        replaceNumberIfNeeded(controller, structure, i, structureType);
+      const structures = structureMatch.map((match) => {
+        return {
+          uri: match.term.value,
+          range: match.nodes[0],
+        };
+      });
+      if (!structures.length) return true;
+      for (let i = 0; i < structures.length; i++) {
+        replaceNumberIfNeeded(controller, structures[i], i, structureType);
       }
     }
     return true;
@@ -54,28 +54,22 @@ export default function recalculateStructureNumbers(
 
 function replaceNumberIfNeeded(
   controller: ProseController,
-  structure: { from: number; to: number },
+  structure: { range: { from: number; to: number }; uri: string },
   index: number,
   structureType: StructureSpec
 ) {
   const numberPredicate =
     structureType.numberPredicate || defaults.numberPredicate;
-  const resource = unwrap(
-    getRdfaAttribute(
-      unwrap(controller.state.doc.nodeAt(structure.from)),
-      'resource'
-    ).pop()
-  );
   let structureNumberObjectNode = controller.datastore
-    .match(`>${resource}`, `>${numberPredicate}`)
+    .match(`>${structure.uri}`, `>${numberPredicate}`)
     .asObjectNodeMapping()
     .single();
   structureNumberObjectNode = expect(
-    `${resource} expected to have the predicate ${numberPredicate}`,
+    `${structure.uri} expected to have the predicate ${numberPredicate}`,
     structureNumberObjectNode
   );
   const structureNumber = structureNumberObjectNode.term.value;
-  const structureNumberElement = [...structureNumberObjectNode.nodes][0];
+  const structureNumberElement = structureNumberObjectNode.nodes[0];
   if (!structureNumberElement) {
     throw new Error('Could not find object node');
   }
@@ -87,13 +81,8 @@ function replaceNumberIfNeeded(
   }
   if (structureNumber !== structureNumberExpected) {
     const { from, to } = structureNumberElement;
-    const insertRange = { from: from + 1, to: to - 1 };
     controller.withTransaction((tr) => {
-      return tr.insertText(
-        String(structureNumberExpected),
-        insertRange.from,
-        insertRange.to
-      );
+      return tr.insertText(String(structureNumberExpected), from, to);
     });
   }
 }
