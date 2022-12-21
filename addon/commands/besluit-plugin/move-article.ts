b0a985a11466fca97e353361c84b13fda16952d6
Elena Poelman <elena.poelman@redpencil.io>
2022-12-21 12:50:42 +0100

    remove console logs

--- addon/commands/besluit-plugin/move-article.ts
+++ addon/commands/besluit-plugin/move-article.ts
@@ -17,7 +17,6 @@ export default function moveArticleCommand(
 ): Command {
   return (_state, dispatch) => {
     const articles = getArticleNodesForBesluit(controller, besluitUri);
-    console.log('ARTICLES: ', articles);
     if (!articles) {
       return false;
     }
