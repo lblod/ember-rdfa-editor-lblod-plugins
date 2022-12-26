50b4d035b718b965362fa9ee590cf487bc439074
Oscar <lagartoverde97@gmail.com>
2022-12-26 15:04:21 +0100

    Remove paragraphs from toc

--- addon/constants.ts
+++ addon/constants.ts
@@ -14,13 +14,6 @@ export const TABLE_OF_CONTENTS_DEFAULT_CONFIG: TableOfContentsConfig = [
       predicate: 'https://say.data.gift/ns/heading',
     },
   },
-  {
-    sectionPredicate: [
-      'https://say.data.gift/ns/hasParagraph',
-      'say:hasParagraph',
-    ],
-    value: '§',
-  },
 ];
 
 export const ZONAL_URI =
