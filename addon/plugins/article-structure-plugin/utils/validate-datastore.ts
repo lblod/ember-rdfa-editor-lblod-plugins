9149892b0c3a12e98e59fbd26c68d44487027fbe
Oscar <lagartoverde97@gmail.com>
2022-12-28 14:50:45 +0100

    disable ts errors on datastore validation

--- addon/plugins/article-structure-plugin/utils/validate-datastore.ts
+++ addon/plugins/article-structure-plugin/utils/validate-datastore.ts
@@ -10,8 +10,11 @@ export default async function validateDatastore(
   datastore: ProseStore,
   shaclConstraint: string
 ) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   const s = new Readable();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   s.push(shaclConstraint);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   s.push(null);
   const parser = new ParserN3({ factory });
   const shapes = await factory.dataset().import(parser.import(s));
