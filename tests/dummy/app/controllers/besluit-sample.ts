888efdd01120e026f97237bead636709d2a86605
Elena Poelman <elena.poelman@redpencil.io>
2023-01-10 17:47:54 +0100

    add intl to date placeholders

--- tests/dummy/app/controllers/besluit-sample.ts
+++ tests/dummy/app/controllers/besluit-sample.ts
@@ -59,52 +59,63 @@ import { setupCitationPlugin } from '@lblod/ember-rdfa-editor-lblod-plugins/plug
 import { invisible_rdfa } from '@lblod/ember-rdfa-editor/nodes/inline-rdfa';
 import sampleData from '@lblod/ember-rdfa-editor/config/sample-data';
 import { date } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/rdfa-date-plugin/nodes';
+import IntlService from 'ember-intl/services/intl';
 const citation = setupCitationPlugin();
-const nodes = {
-  doc,
-  paragraph,
-
-  repaired_block,
-
-  list_item,
-  ordered_list,
-  bullet_list,
-  placeholder,
-  ...tableNodes({ tableGroup: 'block', cellContent: 'inline*' }),
-  date,
-  heading,
-  blockquote,
-
-  horizontal_rule,
-  code_block,
-
-  text,
-
-  image,
-
-  hard_break,
-  block_rdfa,
-  invisible_rdfa,
-};
-const marks = {
-  citation: citation.marks.citation,
-  inline_rdfa,
-  link,
-  em,
-  strong,
-  underline,
-  strikethrough,
-};
-const dummySchema = new Schema({ nodes, marks });
 
 export default class BesluitSampleController extends Controller {
   @service declare importRdfaSnippet: importRdfaSnippet;
+  @service declare intl: IntlService;
   prefixes = {
     ext: 'http://mu.semte.ch/vocabularies/ext/',
     mobiliteit: 'https://data.vlaanderen.be/ns/mobiliteit#',
     dct: 'http://purl.org/dc/terms/',
     say: 'https://say.data.gift/ns/',
   };
+
+  get schema() {
+    return new Schema({
+      nodes: {
+        doc,
+        paragraph,
+
+        repaired_block,
+
+        list_item,
+        ordered_list,
+        bullet_list,
+        placeholder,
+        ...tableNodes({ tableGroup: 'block', cellContent: 'inline*' }),
+        date: date({
+          placeholder: {
+            insertDate: this.intl.t('date-plugin.insert.date'),
+            insertDateTime: this.intl.t('date-plugin.insert.datetime'),
+          },
+        }),
+        heading,
+        blockquote,
+
+        horizontal_rule,
+        code_block,
+
+        text,
+
+        image,
+
+        hard_break,
+        block_rdfa,
+        invisible_rdfa,
+      },
+      marks: {
+        citation: citation.marks.citation,
+        inline_rdfa,
+        link,
+        em,
+        strong,
+        underline,
+        strikethrough,
+      },
+    });
+  }
 
   @tracked rdfaEditor?: ProseController;
   @tracked nodeViews: (
@@ -127,7 +138,6 @@ export default class BesluitSampleController extends Controller {
     roadSignRegulationWidget,
     templateVariableWidget,
   ];
-  schema: Schema = dummySchema;
 
   @action
   setPrefixes(element: HTMLElement) {
