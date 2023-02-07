c92874b8f3385e8b7e8abf7794b5cf8d5149299e
Elena Poelman <elena.poelman@redpencil.io>
2023-02-07 12:44:48 +0100

    initialize __rdfaId when creating citation mark

--- addon/plugins/citation-plugin/utils/cited-text.ts
+++ addon/plugins/citation-plugin/utils/cited-text.ts
@@ -1,6 +1,7 @@
 import { PNode } from '@lblod/ember-rdfa-editor';
 import { CitationSchema } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/citation-plugin';
 import { unwrap } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/option';
+import { v4 as uuid } from 'uuid';
 
 export function citedText(
   schema: CitationSchema,
@@ -12,6 +13,7 @@ export function citedText(
       href: uri,
       property: 'eli:cites',
       typeof: 'eli:LegalExpression',
+      __rdfaId: uuid(),
     }),
   ]);
 }
