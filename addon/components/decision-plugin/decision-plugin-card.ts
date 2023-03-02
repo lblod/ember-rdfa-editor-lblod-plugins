12000a3e7d011acb1c3c2d21aee2933a7759dc61
abeforgit <arnebertrand@gmail.com>
2023-03-02 14:26:34 +0100

    refactor(decision-plugin): rename besluit-plugin to decision-plugin

--- addon/components/decision-plugin/decision-plugin-card.ts
+++ addon/components/decision-plugin/decision-plugin-card.ts
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { insertTitle } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/besluit-plugin/commands';
+import { insertTitle } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/decision-plugin/commands';
 import { SayController } from '@lblod/ember-rdfa-editor';
 import { inject as service } from '@ember/service';
 import IntlService from 'ember-intl/services/intl';
@@ -9,7 +9,7 @@ type Args = {
   controller: SayController;
 };
 
-export default class BesluitPluginCardComponent extends Component<Args> {
+export default class DecisionPluginCard extends Component<Args> {
   @service declare intl: IntlService;
   get controller() {
     return this.args.controller;
--- app/components/besluit-plugin/besluit-plugin-card.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@lblod/ember-rdfa-editor-lblod-plugins/components/besluit-plugin/besluit-plugin-card';
