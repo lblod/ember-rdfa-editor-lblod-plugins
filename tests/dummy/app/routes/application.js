deab903ec42de45c8e0d4cdba9e0ba5d31f3d71c
abeforgit <arnebertrand@gmail.com>
2023-04-07 17:13:29 +0200

    fix(build): change application route to js
    due to https://github.com/typed-ember/ember-cli-typescript/issues/780

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,12 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import IntlService from 'ember-intl/services/intl';
-import Transition from '@ember/routing/transition';
 
 export default class ApplicationRoute extends Route {
-  @service declare intl: IntlService;
+  @service intl;
 
-  beforeModel(transition: Transition): Promise<unknown> | void {
+  beforeModel(transition) {
     const userLocale = navigator.language || navigator.languages[0];
     this.intl.setLocale([userLocale, 'nl-BE']);
     return super.beforeModel(transition);
