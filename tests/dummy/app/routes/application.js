543de67afef680012bab92500d097f2f56b38593
abeforgit <arnebertrand@gmail.com>
2023-04-07 17:15:21 +0200

    docs(route): warn against chaging route to typescript

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+// DO NOT CHANGE THIS FILE TO TYPESCRIPT
+// see https://github.com/typed-ember/ember-cli-typescript/issues/780
+// it conflicts with ember-intl's application route
 export default class ApplicationRoute extends Route {
   @service intl;
 
