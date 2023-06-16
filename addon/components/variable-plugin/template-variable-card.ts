82f74eaae0f435bf2688063e324d75ff7b2adcb8
Elena Poelman <elena.poelman@redpencil.io>
2023-06-16 11:47:26 +0200

    refactor template-variable-card component

--- addon/components/variable-plugin/template-variable-card.ts
+++ addon/components/variable-plugin/template-variable-card.ts
@@ -1,34 +1,35 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { task } from 'ember-concurrency';
 import { SayController } from '@lblod/ember-rdfa-editor';
 import {
   CodeListOption,
   fetchCodeListOptions,
 } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin/utils/fetch-data';
-import { MULTI_SELECT_CODELIST_TYPE } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin/utils/constants';
 import { findParentNodeOfType } from '@curvenote/prosemirror-utils';
-import { NodeSelection, PNode, ProseParser } from '@lblod/ember-rdfa-editor';
+import { NodeSelection, ProseParser } from '@lblod/ember-rdfa-editor';
 import { ZONAL_URI } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/roadsign-regulation-plugin/utils/constants';
 import { unwrap } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/option';
 import { TemplateVariablePluginOptions } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin';
-
+import { trackedFunction } from 'ember-resources/util/function';
+import { MULTI_SELECT_CODELIST_TYPE } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin/utils/constants';
+//@ts-expect-error tracked-toolbox has no published types yet
+import { trackedReset } from 'tracked-toolbox';
 type Args = {
   controller: SayController;
   options: TemplateVariablePluginOptions;
 };
 export default class EditorPluginsTemplateVariableCardComponent extends Component<Args> {
-  @tracked variableOptions: CodeListOption[] = [];
-  @tracked selectedVariableOption?: CodeListOption | CodeListOption[];
-  @tracked selectedVariable: { pos: number; node: PNode } | undefined;
-  @tracked showCard = false;
-  @tracked multiSelect = false;
-  @tracked label?: string;
-  mappingUri?: string;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @trackedReset('variableOptions.value') selectedVariableOption?:
+    | CodeListOption
+    | CodeListOption[];
 
   get controller() {
     return this.args.controller;
+  }
+
+  get schema() {
+    return this.args.controller.schema;
   }
 
   @action
@@ -44,7 +45,7 @@ export default class EditorPluginsTemplateVariableCardComponent extends Componen
     } else {
       htmlToInsert = unwrap(this.selectedVariableOption.value);
     }
-    htmlToInsert = this.wrapVariableInHighlight(htmlToInsert);
+    htmlToInsert = wrapVariableInHighlight(htmlToInsert);
     const domParser = new DOMParser();
     const htmlNode = domParser.parseFromString(htmlToInsert, 'text/html');
     const contentFragment = ProseParser.fromSchema(
@@ -64,100 +65,91 @@ export default class EditorPluginsTemplateVariableCardComponent extends Componen
     );
   }
 
-  wrapVariableInHighlight(text: string) {
-    return text.replace(
-      /\$\{(.+?)\}/g,
-      '<span class="mark-highlight-manual">${$1}</span>'
-    );
-  }
-
-  @action
-  selectionChanged() {
-    this.showCard = false;
-    this.selectedVariableOption = undefined;
+  get selectedVariable() {
     const { selection } = this.controller.mainEditorState;
     if (
       selection instanceof NodeSelection &&
-      selection.node.type === this.controller.schema.nodes.variable
+      (selection.node.type === this.schema.nodes.codelist_variable ||
+        selection.node.type === this.schema.nodes.location_variable)
     ) {
-      const variable = {
+      return {
         node: selection.node,
         pos: selection.from,
       };
-      this.selectedVariable = variable;
-      if (variable) {
-        const type = variable.node.attrs.type as string;
-        if (type === 'codelist') {
-          const source =
-            (variable.node.attrs.source as string | undefined) ??
-            this.args.options.endpoint;
-          const codelistURI = variable.node.attrs.codelistResource as string;
-          void this.fetchCodeListOptions.perform(source, codelistURI);
-          this.showCard = true;
-          this.label = variable.node.attrs.label as string;
-        } else if (type === 'location') {
-          const source =
-            (variable.node.attrs.source as string | undefined) ??
-            this.args.options.endpoint;
-          const roadSignRegulation = findParentNodeOfType(
-            this.controller.schema.nodes.roadsign_regulation
-          )(selection);
-          const zonalityUri = roadSignRegulation?.node.attrs.zonality as
-            | string
-            | undefined;
-          this.label = variable.node.attrs.label as string;
-          if (zonalityUri === ZONAL_URI) {
-            void this.fetchCodeListOptions.perform(
-              source,
-              this.args.options.zonalLocationCodelistUri,
-              true
-            );
-          } else {
-            void this.fetchCodeListOptions.perform(
-              source,
-              this.args.options.nonZonalLocationCodelistUri,
-              true
-            );
-          }
-
-          this.showCard = true;
-        }
-      }
+    } else {
+      return;
     }
+  }
+
+  get label() {
+    return this.selectedVariable?.node.attrs.label as string | undefined;
+  }
+
+  get showCard() {
+    return !!this.selectedVariable;
+  }
+
+  get isLocation() {
+    return (
+      this.selectedVariable?.node.type === this.schema.nodes.location_variable
+    );
+  }
+
+  variableOptions = trackedFunction(this, async () => {
+    if (this.selectedVariable) {
+      const source =
+        (this.selectedVariable.node.attrs.source as string | undefined) ??
+        this.args.options.endpoint;
+      if (this.isLocation) {
+        const { selection } = this.controller.mainEditorState;
+        const roadSignRegulation = findParentNodeOfType(
+          this.controller.schema.nodes.roadsign_regulation
+        )(selection);
+        const zonalityUri = roadSignRegulation?.node.attrs.zonality as
+          | string
+          | undefined;
+        const variableOptions = await fetchCodeListOptions(
+          source,
+          zonalityUri === ZONAL_URI
+            ? this.args.options.zonalLocationCodelistUri
+            : this.args.options.nonZonalLocationCodelistUri
+        );
+        variableOptions.options = variableOptions.options.map((option) => ({
+          label: option.label,
+          value: wrapInLocation(unwrap(option.value)),
+        }));
+        return variableOptions;
+      } else {
+        const codelistURI = this.selectedVariable.node.attrs
+          .codelistResource as string;
+        return fetchCodeListOptions(source, codelistURI);
+      }
+    } else {
+      return;
+    }
+  });
+
+  get multiSelect() {
+    return this.variableOptions.value?.type === MULTI_SELECT_CODELIST_TYPE;
   }
 
   @action
   updateVariableOption(variableOption: CodeListOption | CodeListOption[]) {
     this.selectedVariableOption = variableOption;
   }
+}
 
-  fetchCodeListOptions = task(
-    async (endpoint: string, codelistUri: string, isLocation?: boolean) => {
-      const { type, options } = await fetchCodeListOptions(
-        endpoint,
-        codelistUri
-      );
-      if (isLocation) {
-        this.variableOptions = options.map((option) => ({
-          label: option.label,
-          value: this.wrapInLocation(unwrap(option.value)),
-        }));
-      } else {
-        this.variableOptions = options;
-      }
-      if (type === MULTI_SELECT_CODELIST_TYPE) {
-        this.multiSelect = true;
-      } else {
-        this.multiSelect = false;
-      }
-    }
+function wrapVariableInHighlight(text: string) {
+  return text.replace(
+    /\$\{(.+?)\}/g,
+    '<span class="mark-highlight-manual">${$1}</span>'
   );
+}
 
-  wrapInLocation(value: string) {
-    return `
-      <span property="https://data.vlaanderen.be/ns/mobiliteit#plaatsbepaling">
-        ${value}
-      </span>
-    `;
-  }
+function wrapInLocation(value: string) {
+  return `
+    <span property="https://data.vlaanderen.be/ns/mobiliteit#plaatsbepaling">
+      ${value}
+    </span>
+  `;
 }
