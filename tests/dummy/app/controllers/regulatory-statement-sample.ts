19e5ef746baebd1556edac5ec522bce3c9062329
abeforgit <arnebertrand@gmail.com>
2023-07-06 11:29:27 +0200

    fix(deps): revert dev dependency on editor back to 3.10.0

--- tests/dummy/app/controllers/regulatory-statement-sample.ts
+++ tests/dummy/app/controllers/regulatory-statement-sample.ts
@@ -12,7 +12,6 @@ import {
 } from '@lblod/ember-rdfa-editor/plugins/text-style';
 import {
   block_rdfa,
-  docWithConfig,
   hard_break,
   horizontal_rule,
   invisible_rdfa,
@@ -85,10 +84,10 @@ export default class RegulatoryStatementSampleController extends Controller {
   get schema() {
     return new Schema({
       nodes: {
-        doc: docWithConfig({
+        doc: {
           content:
             'table_of_contents? ((chapter|block)+|(title|block)+|(article|block)+)',
-        }),
+        },
         paragraph,
 
         repaired_block,
@@ -233,7 +232,7 @@ export default class RegulatoryStatementSampleController extends Controller {
     const presetContent =
       localStorage.getItem('EDITOR_CONTENT') ??
       `<div resource='http://localhost/test' typeof='say:DocumentContent'>Insert here</div>`;
-    controller.initialize(presetContent);
+    controller.setHtmlContent(presetContent);
     const editorDone = new CustomEvent('editor-done');
     window.dispatchEvent(editorDone);
   }
