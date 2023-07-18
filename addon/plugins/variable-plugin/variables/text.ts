eed14adaab3353e9a9697318e1131a30f603fd77
Elena Poelman <elena.poelman@redpencil.io>
2023-07-18 11:03:58 +0200

    addition of seperate text variable node-spec

---
+++ addon/plugins/variable-plugin/variables/text.ts
@@ -0,0 +1,98 @@
+import {
+  DCT,
+  EXT,
+} from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
+import {
+  createEmberNodeSpec,
+  createEmberNodeView,
+  EmberNodeConfig,
+} from '@lblod/ember-rdfa-editor/utils/ember-node';
+import { v4 as uuidv4 } from 'uuid';
+import { DOMOutputSpec, PNode } from '@lblod/ember-rdfa-editor';
+import {
+  isVariable,
+  parseLabel,
+  parseVariableInstance,
+  parseVariableType,
+} from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin/utils/attribute-parsers';
+
+const CONTENT_SELECTOR = `span[property~='${EXT('content').prefixed}'],
+                          span[property~='${EXT('content').full}']`;
+
+const parseDOM = [
+  {
+    tag: 'span',
+    getAttrs: (node: HTMLElement) => {
+      if (
+        isVariable(node) &&
+        node.querySelector(CONTENT_SELECTOR) &&
+        parseVariableType(node) === 'text'
+      ) {
+        const mappingResource = node.getAttribute('resource');
+        if (!mappingResource) {
+          return false;
+        }
+        const variableInstance = parseVariableInstance(node);
+        const label = parseLabel(node);
+        return {
+          variableInstance:
+            variableInstance ?? `http://data.lblod.info/variables/${uuidv4()}`,
+          mappingResource,
+          label,
+        };
+      }
+
+      return false;
+    },
+    contentElement: CONTENT_SELECTOR,
+  },
+];
+
+const toDOM = (node: PNode): DOMOutputSpec => {
+  const { mappingResource, variableInstance, label } = node.attrs;
+  return [
+    'span',
+    {
+      resource: mappingResource as string,
+      typeof: EXT('Mapping').prefixed,
+      'data-label': label as string,
+    },
+    [
+      'span',
+      { property: EXT('instance'), resource: variableInstance as string },
+    ],
+    ['span', { property: DCT('type').prefixed, content: 'text' }],
+    [
+      'span',
+      {
+        property: EXT('content').prefixed,
+      },
+      0,
+    ],
+  ];
+};
+
+export const emberNodeConfig: EmberNodeConfig = {
+  name: 'text-variable',
+  componentPath: 'variable-plugin/variable',
+  inline: true,
+  group: 'inline',
+  content: 'inline*',
+  atom: true,
+  recreateUri: true,
+  uriAttributes: ['variableInstance'],
+  draggable: false,
+  needsFFKludge: true,
+  attrs: {
+    mappingResource: {},
+    variableInstance: {},
+    label: {
+      default: 'text',
+    },
+  },
+  toDOM,
+  parseDOM,
+};
+
+export const text_variable = createEmberNodeSpec(emberNodeConfig);
+export const textVariableView = createEmberNodeView(emberNodeConfig);
