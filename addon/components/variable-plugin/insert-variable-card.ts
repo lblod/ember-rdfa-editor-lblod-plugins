6c55ed6e7b888db28b145cfff2a97aa6aa5c72ec
Elena Poelman <elena.poelman@redpencil.io>
2023-07-20 13:05:12 +0200

    simplify showCard of insert-variable-card

--- addon/components/variable-plugin/insert-variable-card.ts
+++ addon/components/variable-plugin/insert-variable-card.ts
@@ -2,8 +2,6 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { SayController } from '@lblod/ember-rdfa-editor';
-import { findParentNodeOfType } from '@curvenote/prosemirror-utils';
-import { NodeSelection } from '@lblod/ember-rdfa-editor';
 import { service } from '@ember/service';
 import IntlService from 'ember-intl/services/intl';
 
@@ -35,20 +33,6 @@ export default class EditorPluginsInsertCodelistCardComponent extends Component<
   }
 
   get showCard() {
-    if (this.controller.inEmbeddedView) {
-      return false;
-    }
-    const { selection } = this.controller.mainEditorState;
-    if (
-      selection instanceof NodeSelection &&
-      selection.node.type === this.controller.schema.nodes.variable
-    ) {
-      return false;
-    } else {
-      const variable = findParentNodeOfType(
-        this.controller.schema.nodes.variable,
-      )(selection);
-      return !variable;
-    }
+    return !this.controller.inEmbeddedView;
   }
 }
