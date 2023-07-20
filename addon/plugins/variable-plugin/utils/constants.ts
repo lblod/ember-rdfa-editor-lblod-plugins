f7ac57bac4ad4c056917c595d477006188079079
Elena Poelman <elena.poelman@redpencil.io>
2023-07-20 13:04:50 +0200

    remove ununused code

--- addon/plugins/variable-plugin/utils/constants.ts
+++ addon/plugins/variable-plugin/utils/constants.ts
@@ -1,13 +1,2 @@
-import { Attrs, PNode, Schema } from '@lblod/ember-rdfa-editor';
-import { CodeList } from './fetch-data';
-
 export const MULTI_SELECT_CODELIST_TYPE =
   'http://lblod.data.gift/concepts/57C93E12-A02C-4D4B-8B95-666B6701286C';
-
-export const MINIMUM_VALUE_PNODE_KEY = 'minimumValue';
-export const MAXIMUM_VALUE_PNODE_KEY = 'maximumValue';
-export const WRITTEN_NUMBER_PNODE_KEY = 'writtenNumber';
-
-export const MINIMUM_VALUE_HTML_ATTRIBUTE_KEY = 'data-minimum-value';
-export const MAXIMUM_VALUE_HTML_ATTRIBUTE_KEY = 'data-maximum-value';
-export const WRITTEN_NUMBER_HTML_ATTRIBUTE_KEY = 'data-written-number';
