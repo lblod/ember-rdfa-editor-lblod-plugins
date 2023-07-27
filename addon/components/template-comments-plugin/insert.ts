0f88f19e7e88f605898553e657feb72bd1464d10
Ruben <ruben.de.facq@redpencil.io>
2023-07-27 13:46:12 +0200

    only allow insertion of template comment where content allows it

--- addon/components/template-comments-plugin/insert.ts
+++ addon/components/template-comments-plugin/insert.ts
@@ -1,7 +1,8 @@
-import { findParentNode } from '@curvenote/prosemirror-utils';
+import { findParentNode, canInsert } from '@curvenote/prosemirror-utils';
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { Command, SayController } from '@lblod/ember-rdfa-editor';
+import { templateComment } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/template-comments-plugin';
 
 type Args = {
   controller: SayController;
@@ -32,26 +33,47 @@ export default class TemplateCommentsPluginInsertCardComponent extends Component
   insertCommand(): Command {
     return (state, dispatch) => {
       if (!this.controller || this.controller.inEmbeddedView) return false;
-      if (dispatch) {
-        const selection = this.controller.mainEditorState.selection;
-        const parent = findParentNode((node) => {
-          return (
-            node.type !== this.controller.schema.nodes.text &&
-            node.type !== this.controller.schema.nodes.placeholder
-          );
-        })(selection);
-        if (parent) {
-          dispatch(
-            state.tr
-              .replaceRangeWith(
-                parent.start,
-                parent.start,
-                this.schema.nodes.templateComment.createAndFill(),
-              )
-              .scrollIntoView(),
-          );
+
+      const selection = this.controller.mainEditorState.selection;
+      const parent = findParentNode((node) => {
+        return (
+          node.type !== this.controller.schema.nodes.text &&
+          node.type !== this.controller.schema.nodes.placeholder
+        );
+      })(selection);
+
+      const newTemplateNode = this.schema.nodes.templateComment.createAndFill();
+      if (!parent) {
+        // if no parent, selection is somewhere at the top of doc, so insert at start of doc
+        if (dispatch) {
+          const transaction = state.tr
+            .insert(0, newTemplateNode)
+            .scrollIntoView();
+
+          dispatch(transaction);
           this.controller.focus();
         }
+        return true;
+      }
+
+      // get the parent of parent
+      // check what content is allowed (contentMatchAt) by using the "index"
+      // check if template comment is allowed as comment.
+      // In principle this should be the same as `CanAppend`, but this seems to work better.
+      const insertPos = parent.pos;
+      const resolvedPos = state.doc.resolve(insertPos);
+      const parentAboveInsertion = resolvedPos.parent;
+      const indexInParent = resolvedPos.index();
+      const allowedContent = parentAboveInsertion.contentMatchAt(indexInParent);
+      const allowed = allowedContent.matchType(newTemplateNode.type);
+      if (!allowed) return false;
+      if (dispatch) {
+        const transaction = state.tr
+          .insert(insertPos, newTemplateNode)
+          .scrollIntoView();
+
+        dispatch(transaction);
+        this.controller.focus();
       }
       return true;
     };
--- addon/plugins/template-comments-plugin/node.ts
+++ addon/plugins/template-comments-plugin/node.ts
@@ -13,7 +13,7 @@ export const emberNodeConfig: () => EmberNodeConfig = () => {
     componentPath: 'template-comments-plugin/template-comment',
     inline: false,
     group: 'block',
-    content: 'block+',
+    content: '(paragraph | list)+',
     draggable: false,
     selectable: true,
     isolating: true,
