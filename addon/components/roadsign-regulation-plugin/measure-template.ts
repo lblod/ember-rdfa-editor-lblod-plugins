a1773e419093ed54be1d25d512390e670b0c3ff3
abeforgit <arnebertrand@gmail.com>
2023-08-07 21:56:37 +0200

    fix(roadsigns): properly pass through endpoint config

--- addon/components/roadsign-regulation-plugin/measure-template.ts
+++ addon/components/roadsign-regulation-plugin/measure-template.ts
@@ -1,9 +1,8 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { task } from 'ember-concurrency';
-import { tracked } from '@glimmer/tracking';
 import includeInstructions from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/roadsign-regulation-plugin/utils/includeInstructions';
 import RoadsignRegistryService from '@lblod/ember-rdfa-editor-lblod-plugins/services/roadsign-registry';
+import { trackedFunction } from 'ember-resources/util/function';
 
 type Args = {
   template: string;
@@ -13,27 +12,22 @@ type Args = {
 };
 export default class MeasureTemplateComponent extends Component<Args> {
   @service declare roadsignRegistry: RoadsignRegistryService;
-  @tracked template = '';
-  endpoint: string;
 
-  constructor(parent: unknown, args: Args) {
-    super(parent, args);
-    this.template = this.args.template;
-    void this.fetchData.perform();
-    this.endpoint = this.args.endpoint;
+  get template() {
+    return this.instructionData.value ?? '';
   }
 
-  fetchData = task(async () => {
+  instructionData = trackedFunction(this, async () => {
     const instructions =
       await this.roadsignRegistry.getInstructionsForMeasure.perform(
         this.args.measure,
-        this.endpoint
+        this.args.endpoint
       );
     const template = includeInstructions(
       this.args.template,
       instructions,
       this.args.annotated
     );
-    this.template = template;
+    return template;
   });
 }
