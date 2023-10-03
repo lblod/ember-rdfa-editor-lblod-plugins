01163d8d45b801a50728b9aeabaf0e52a1cf9a33
Rich <piemonkey@protonmail.ch>
2023-10-03 11:02:21 +0200

    Export instantiateUuids and improve memoization function

--- addon/utils/memoize.ts
+++ addon/utils/memoize.ts
@@ -1,8 +1,16 @@
-export default function memoize(func: (...a: unknown[]) => unknown) {
-  const cache: Record<string, unknown> = {};
+/**
+ * Return memoized version of the passed function.
+ * Memoization is done on the JSON stringified arguments, passing no args, undefined, or any number
+ * of only undefined arguments does not do any caching.
+ */
+export default function memoize<T>(func: (...a: unknown[]) => T) {
+  const cache: Record<string, T> = {};
   return (...args: unknown[]) => {
+    if (args.length === 0 || args.every((arg) => arg === undefined)) {
+      return func(...args);
+    }
     const serializedArgs = JSON.stringify(args);
-    cache[serializedArgs] = cache[serializedArgs] || func(args);
+    cache[serializedArgs] = cache[serializedArgs] || func(...args);
     return cache[serializedArgs];
   };
 }
