a8d11b5eeb0f7b3d3279525fa5a017c536276761
Rich <piemonkey@protonmail.ch>
2023-11-13 10:54:23 +0100

    Copy editable-node sample from editor dummy app

---
+++ tests/dummy/app/controllers/editable-node.ts
@@ -0,0 +1,185 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from 'tracked-built-ins';
+import { Schema } from 'prosemirror-model';
+import {
+  em,
+  strikethrough,
+  strong,
+  subscript,
+  superscript,
+  underline,
+} from '@lblod/ember-rdfa-editor/plugins/text-style';
+import {
+  block_rdfa,
+  docWithConfig,
+  hard_break,
+  horizontal_rule,
+  paragraph,
+  repaired_block,
+  text,
+} from '@lblod/ember-rdfa-editor/nodes';
+import applyDevTools from 'prosemirror-dev-tools';
+import { code } from '@lblod/ember-rdfa-editor/plugins/code/marks/code';
+import {
+  tableKeymap,
+  tableNodes,
+  tablePlugin,
+} from '@lblod/ember-rdfa-editor/plugins/table';
+import { image, imageView } from '@lblod/ember-rdfa-editor/plugins/image';
+import { blockquote } from '@lblod/ember-rdfa-editor/plugins/blockquote';
+import { heading } from '@lblod/ember-rdfa-editor/plugins/heading';
+import { code_block } from '@lblod/ember-rdfa-editor/plugins/code';
+import {
+  bullet_list,
+  list_item,
+  ordered_list,
+} from '@lblod/ember-rdfa-editor/plugins/list';
+import { placeholder } from '@lblod/ember-rdfa-editor/plugins/placeholder';
+import { inline_rdfa } from '@lblod/ember-rdfa-editor/marks';
+import SayController from '@lblod/ember-rdfa-editor/core/say-controller';
+import {
+  link,
+  linkPasteHandler,
+  linkView,
+} from '@lblod/ember-rdfa-editor/plugins/link';
+import { inject as service } from '@ember/service';
+import IntlService from 'ember-intl/services/intl';
+import {
+  createInvisiblesPlugin,
+  hardBreak,
+  heading as headingInvisible,
+  paragraph as paragraphInvisible,
+  space,
+} from '@lblod/ember-rdfa-editor/plugins/invisibles';
+import { highlight } from '@lblod/ember-rdfa-editor/plugins/highlight/marks/highlight';
+import { color } from '@lblod/ember-rdfa-editor/plugins/color/marks/color';
+import { lastKeyPressedPlugin } from '@lblod/ember-rdfa-editor/plugins/last-key-pressed';
+import { firefoxCursorFix } from '@lblod/ember-rdfa-editor/plugins/firefox-cursor-fix';
+import {
+  bullet_list_input_rule,
+  ordered_list_input_rule,
+} from '@lblod/ember-rdfa-editor/plugins/list/input_rules';
+import { inputRules, PluginConfig } from '@lblod/ember-rdfa-editor';
+import { chromeHacksPlugin } from '@lblod/ember-rdfa-editor/plugins/chrome-hacks-plugin';
+import { emberApplication } from '@lblod/ember-rdfa-editor/plugins/ember-application';
+import { getOwner } from '@ember/application';
+import {
+  editableNodePlugin,
+  getActiveEditableNode,
+} from '@lblod/ember-rdfa-editor/plugins/_private/editable-node';
+import DebugInfo from '@lblod/ember-rdfa-editor/components/_private/debug-info';
+import AttributeEditor from '@lblod/ember-rdfa-editor/components/_private/attribute-editor';
+import RdfaEditor from '@lblod/ember-rdfa-editor/components/_private/rdfa-editor';
+
+export default class EditableBlockController extends Controller {
+  DebugInfo = DebugInfo;
+  AttributeEditor = AttributeEditor;
+  RdfaEditor = RdfaEditor;
+
+  @tracked rdfaEditor?: SayController;
+  @service declare intl: IntlService;
+  schema = new Schema({
+    nodes: {
+      doc: docWithConfig({
+        defaultLanguage: 'nl-BE',
+      }),
+      paragraph,
+
+      repaired_block,
+
+      list_item,
+      ordered_list,
+      bullet_list,
+      placeholder,
+      ...tableNodes({ tableGroup: 'block', cellContent: 'block+' }),
+      heading,
+      blockquote,
+
+      horizontal_rule,
+      code_block,
+
+      text,
+
+      image,
+
+      hard_break,
+      block_rdfa,
+      link: link(this.linkOptions),
+    },
+    marks: {
+      inline_rdfa,
+      code,
+      em,
+      strong,
+      underline,
+      strikethrough,
+      subscript,
+      superscript,
+      highlight,
+      color,
+    },
+  });
+
+  get linkOptions() {
+    return {
+      interactive: true,
+    };
+  }
+
+  @tracked plugins: PluginConfig = [
+    firefoxCursorFix(),
+    chromeHacksPlugin(),
+    lastKeyPressedPlugin,
+    tablePlugin,
+    tableKeymap,
+    linkPasteHandler(this.schema.nodes.link),
+    createInvisiblesPlugin(
+      [space, hardBreak, paragraphInvisible, headingInvisible],
+      {
+        shouldShowInvisibles: false,
+      },
+    ),
+    inputRules({
+      rules: [
+        bullet_list_input_rule(this.schema.nodes.bullet_list),
+        ordered_list_input_rule(this.schema.nodes.ordered_list),
+      ],
+    }),
+    emberApplication({ application: getOwner(this) }),
+    editableNodePlugin,
+  ];
+
+  @tracked nodeViews = (controller: SayController) => {
+    return {
+      link: linkView(this.linkOptions)(controller),
+      image: imageView(controller),
+    };
+  };
+
+  get activeNode() {
+    if (this.rdfaEditor) {
+      return getActiveEditableNode(this.rdfaEditor.activeEditorState);
+    }
+    return;
+  }
+
+  get showRdfaBlocks() {
+    return this.rdfaEditor?.showRdfaBlocks;
+  }
+
+  @action
+  rdfaEditorInit(rdfaEditor: SayController) {
+    const presetContent = localStorage.getItem('EDITOR_CONTENT') ?? '';
+    this.rdfaEditor = rdfaEditor;
+    this.rdfaEditor.initialize(presetContent);
+    applyDevTools(rdfaEditor.mainEditorView);
+    const editorDone = new CustomEvent('editor-done');
+    window.dispatchEvent(editorDone);
+  }
+
+  @action
+  togglePlugin() {
+    console.warn('Live toggling plugins is currently not supported');
+  }
+}
