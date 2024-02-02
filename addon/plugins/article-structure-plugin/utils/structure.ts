25996d3ca03f1c26f42065030ad05dcbeb9c2108
Elena Poelman <elena.poelman@redpencil.io>
2024-02-02 10:47:29 +0100

    add non-default subject attribute to structure-specs to prevent automatic instantiation

--- addon/plugins/article-structure-plugin/utils/structure.ts
+++ addon/plugins/article-structure-plugin/utils/structure.ts
@@ -45,6 +45,7 @@ export function constructStructureNodeSpec(config: {
       typeof: {
         default: type.prefixed,
       },
+      subject: {},
     },
     allowSplitByTable: config.allowSplitByTable,
     toDOM(node) {
