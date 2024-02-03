af53acec2b3b72911ea239b7f041480b6e2c3eb1
abeforgit <arnebertrand@gmail.com>
2024-02-03 16:30:40 +0100

    fix: make dummy application route js again

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,16 +1,14 @@
 import Route from '@ember/routing/route';
-import Transition from '@ember/routing/transition';
 import { service } from '@ember/service';
 import { decentLocaleMatch } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/translation';
-import IntlService from 'ember-intl/services/intl';
 
 // DO NOT CHANGE THIS FILE TO TYPESCRIPT
 // see https://github.com/typed-ember/ember-cli-typescript/issues/780
 // it conflicts with ember-intl's application route
 export default class ApplicationRoute extends Route {
-  @service declare intl: IntlService;
+  @service intl;
 
-  beforeModel(transition: Transition) {
+  beforeModel(transition) {
     const userLocales = decentLocaleMatch(
       navigator.languages,
       this.intl.locales,
