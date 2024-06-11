4d607462eca5f46811d1d2f3e258fb7d1c0df922
abeforgit <arnebertrand@gmail.com>
2024-06-11 16:20:13 +0200

    fix(structure): correctly position cursor after moving

--- addon/plugins/structure-plugin/move-structure.ts
+++ addon/plugins/structure-plugin/move-structure.ts
@@ -1,5 +1,6 @@
 import {
   Command,
+  NodeSelection,
   PNode,
   ResolvedPos,
   TextSelection,
@@ -29,7 +30,7 @@ export function moveStructure(direction: 'up' | 'down'): Command {
       if (currentPos <= 1) {
         return false;
       }
-      const previousStructure =
+      const previousStructurePos =
         findNodePosUp(doc, pos, ($pos: ResolvedPos) => {
           const node = $pos.nodeAfter;
           if (node) {
@@ -37,27 +38,41 @@ export function moveStructure(direction: 'up' | 'down'): Command {
           }
           return false;
         }).next().value ?? null;
-      if (isNone(previousStructure)) {
+      if (isNone(previousStructurePos)) {
         return false;
       } else {
         if (dispatch) {
           const transaction = state.tr;
+          // we are moving the structure up, so if we delete it first, it won't affect the positions
+          // before it
           transaction.delete(pos, pos + node.nodeSize);
-          transaction.insert(previousStructure, node);
+
+          // previousStructurePos is the position right BEFORE the previous structure,
+          // so inserting there is correct
+          transaction.insert(previousStructurePos, node);
+          // after updating we have to recalculate the numbers
           const { transaction: newTr } = transactionCombinator(
             state,
             transaction,
           )([recalculateNumbers]);
 
-          const newPos = newTr.mapping.map(previousStructure);
-          const $newPos = newTr.doc.resolve(newPos);
-          newTr.setSelection(new TextSelection($newPos));
+          // previousStructurePos should now point to the position right before our moved structure
+          // so we can simply add 1 to get the first position inside of it, and for the end
+          // we add the nodesize and subtract one
+          newTr.setSelection(
+            TextSelection.create(
+              newTr.doc,
+              previousStructurePos + 1,
+              previousStructurePos + node.nodeSize - 1,
+            ),
+          );
           dispatch(newTr);
         }
         return true;
       }
     } else {
-      const nextStructure =
+      // findNodePosUp and findNodePosDown have a different interface (historic reasons)
+      const nextStructureParentPos =
         findNodePosDown(
           doc,
           doc.resolve(pos + node.nodeSize),
@@ -65,29 +80,38 @@ export function moveStructure(direction: 'up' | 'down'): Command {
             return parent.type.name === 'structure';
           },
         ).next().value ?? null;
-      if (isNone(nextStructure)) {
+      if (isNone(nextStructureParentPos)) {
         return false;
       }
+      // findNodePosDown has a weird quirk where it can only find positions IN nodes
+      // cause the filter function only receives the position's parent
+      // so we need to step back one here (or make a way more complicated filter)
+      const nextStructurePos = nextStructureParentPos - 1;
       if (dispatch) {
         const transaction = state.tr;
-        const $structurePos = doc.resolve(nextStructure);
+        const $structurePos = doc.resolve(nextStructurePos);
         const nextStructureNode = unwrap($structurePos.nodeAfter);
 
-        transaction.insert(
-          nextStructure + nextStructureNode.nodeSize + 1,
-          node,
-        );
         transaction.delete(pos, pos + node.nodeSize);
+        const insertPos = transaction.mapping.map(
+          nextStructurePos + nextStructureNode.nodeSize,
+        );
+        transaction.insert(insertPos, node);
 
         const { transaction: newTr } = transactionCombinator(
           state,
           transaction,
         )([recalculateNumbers]);
-        const newPos = newTr.mapping.map(
-          nextStructure + nextStructureNode.nodeSize,
+
+        // since we've deleted something before the insert position, we have to map
+        // the positions through the transaction to find the moved node's new position
+        newTr.setSelection(
+          TextSelection.create(
+            newTr.doc,
+            insertPos + 1,
+            insertPos + node.nodeSize - 1,
+          ),
         );
-        const $newPos = newTr.doc.resolve(newPos);
-        newTr.setSelection(new TextSelection($newPos, $newPos));
         dispatch(newTr);
       }
 
