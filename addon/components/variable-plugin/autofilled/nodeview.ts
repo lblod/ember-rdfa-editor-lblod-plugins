584a610da8bc6ef42792a0ef3200c73d74cef3e3
Oscar <lagartoverde97@gmail.com>
2024-09-03 11:38:42 +0200

    solved weird bug

--- addon/components/variable-plugin/autofilled/nodeview.ts
+++ addon/components/variable-plugin/autofilled/nodeview.ts
@@ -51,6 +51,7 @@ export default class AutoFilledVariableNodeViewComponent extends Component<Ember
   }
   @action
   async didInsert() {
+    console.log('inserting')
     const autofillKey = this.args.node.attrs.autofillKey;
     const autofilledValues = this.args.node.attrs.autofilledValues;
     const value = autofilledValues[autofillKey]
@@ -67,18 +68,18 @@ export default class AutoFilledVariableNodeViewComponent extends Component<Ember
         schema.text(value),
       )
       console.log(this.args.node.attrs.convertToString)
-      if(this.args.node.attrs.convertToString) {
+      if(false && this.args.node.attrs.convertToString) {
+        console.log('not here')
         this.args.controller.withTransaction((tr: Transaction) => {
           tr.replaceRangeWith(nodePos, nodePos+nodeSize , valueNode)
           return tr;
         })
       } else {
         console.log('replacing inner')
-        if(!this.innerView) return
         this.args.controller.withTransaction((tr: Transaction) => {
-          tr.replaceRangeWith(0, this.innerView.state.doc.nodeSize - 1, valueNode)
+          tr.replaceRangeWith(nodePos + 1, nodePos+nodeSize -1  , valueNode)
           return tr;
-        }, {view: this.innerView})
+        })
       }
     }
   }
