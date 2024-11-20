ae69232e33c64d29f31d81c8c7f269de9912836e
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2024-11-20 14:03:03 +0100

    use getOutgoingTriple function

--- addon/plugins/variable-plugin/utils/recreate-variable-uris.ts
+++ addon/plugins/variable-plugin/utils/recreate-variable-uris.ts
@@ -1,8 +1,8 @@
 import { EXT } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
 import { sayDataFactory } from '@lblod/ember-rdfa-editor/core/say-data-factory';
-import { OutgoingTriple } from '@lblod/ember-rdfa-editor/core/rdfa-processor';
 import { v4 as uuidv4 } from 'uuid';
 import { Attrs } from '@lblod/ember-rdfa-editor';
+import { getOutgoingTriple } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/namespace';
 
 export function recreateVariableUris(nodeAttrs: Attrs) {
   const attrs = { ...nodeAttrs };
@@ -19,25 +19,18 @@ export function recreateVariableUris(nodeAttrs: Attrs) {
   ) {
     attrs.subject = `http://data.lblod.info/mappings/${uuidv4()}`;
   }
-  attrs.properties = (attrs.properties as OutgoingTriple[]).map((prop) => {
-    if (prop.predicate === EXT('instance').full) {
-      let recreatedUri;
-      if (
-        prop.object.value.includes(
-          'http://data.lblod.info/variables/--ref-uuid4-',
-        )
-      ) {
-        recreatedUri = `http://data.lblod.info/variables/--ref-uuid4-${uuidv4()}`;
-      } else {
-        recreatedUri = `http://data.lblod.info/variables/${uuidv4()}`;
-      }
-      return {
-        predicate: prop.predicate,
-        object: sayDataFactory.namedNode(recreatedUri),
-      };
-    }
-
-    return prop;
-  });
+  const instanceTriple = getOutgoingTriple(attrs, EXT('instance'));
+  if (!instanceTriple) return attrs;
+  let recreatedUri;
+  if (
+    instanceTriple.object.value.includes(
+      'http://data.lblod.info/variables/--ref-uuid4-',
+    )
+  ) {
+    recreatedUri = `http://data.lblod.info/variables/--ref-uuid4-${uuidv4()}`;
+  } else {
+    recreatedUri = `http://data.lblod.info/variables/${uuidv4()}`;
+  }
+  instanceTriple.object = sayDataFactory.namedNode(recreatedUri);
   return attrs;
 }
