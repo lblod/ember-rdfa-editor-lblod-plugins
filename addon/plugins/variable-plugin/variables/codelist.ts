aa1e9812d424a3e5bbe6afc5677e1fe89457e986
Nordine Bittich <contact@bittich.be>
2024-12-18 15:25:45 +0100

    modify plugin for codelist, date & text

--- addon/plugins/variable-plugin/variables/codelist.ts
+++ addon/plugins/variable-plugin/variables/codelist.ts
@@ -5,6 +5,7 @@ import {
 import {
   DCT,
   EXT,
+  MOBILITEIT,
   RDF,
 } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
 import {
@@ -23,6 +24,7 @@ import { sayDataFactory } from '@lblod/ember-rdfa-editor/core/say-data-factory';
 import {
   hasRdfaVariableType,
   isVariable,
+  isVariableNewModel,
   parseLabel,
   parseSelectionStyle,
   parseVariableInstance,
@@ -36,6 +38,8 @@ import { recreateVariableUris } from '../utils/recreate-variable-uris';
 
 const CONTENT_SELECTOR = `span[property~='${EXT('content').prefixed}'],
                           span[property~='${EXT('content').full}']`;
+const CONTENT_SELECTOR_NEW_MODEL = `span[property~='${RDF('value').prefixed}'],
+                          span[property~='${RDF('value').full}']`;
 const rdfaAware = true;
 
 const parseDOM = [
@@ -98,7 +102,7 @@ const parseDOM = [
             predicate: EXT('instance').full,
             object: sayDataFactory.namedNode(
               variableInstance ??
-                `http://data.lblod.info/variables/${uuidv4()}`,
+              `http://data.lblod.info/variables/${uuidv4()}`,
             ),
           },
           {
@@ -140,6 +144,83 @@ const parseDOM = [
       return false;
     },
     contentElement: CONTENT_SELECTOR,
+  },
+
+  {
+    tag: 'span',
+    getAttrs: (node: HTMLElement) => {
+      if (
+        isVariableNewModel(node) &&
+        node.querySelector(CONTENT_SELECTOR_NEW_MODEL) &&
+        parseVariableType(node) === 'codelist'
+      ) {
+        const mappingResource = node.getAttribute('resource');
+        if (!mappingResource) {
+          return false;
+        }
+        const variableInstance = parseVariableInstance(node);
+
+        const source = parseVariableSource(node);
+        const label = parseLabel(node);
+        const selectionStyle = parseSelectionStyle(node);
+        const codelistSpan = [...node.children].find((el) =>
+          hasRDFaAttribute(el, 'property', EXT('codelist')),
+        );
+        const codelistResource =
+          codelistSpan?.getAttribute('resource') ??
+          codelistSpan?.getAttribute('content');
+
+        const properties = [
+          {
+            predicate: RDF('type').full,
+            object: sayDataFactory.namedNode(MOBILITEIT('Variabele').full),
+          },
+          {
+            predicate: EXT('instance').full,
+            object: sayDataFactory.namedNode(
+              variableInstance ??
+              `http://data.lblod.info/variables/${uuidv4()}`,
+            ),
+          },
+          {
+            predicate: DCT('type').full,
+            object: sayDataFactory.literal('codelist'),
+          },
+          {
+            predicate: RDF('value').full,
+            object: sayDataFactory.contentLiteral(),
+          },
+        ];
+        if (label) {
+          properties.push({
+            predicate: EXT('label').full,
+            object: sayDataFactory.literal(label),
+          });
+        }
+        if (codelistResource) {
+          properties.push({
+            predicate: EXT('codelist').full,
+            object: sayDataFactory.namedNode(codelistResource),
+          });
+        }
+        if (source) {
+          properties.push({
+            predicate: DCT('source').full,
+            object: sayDataFactory.namedNode(source),
+          });
+        }
+        return {
+          selectionStyle,
+          subject: mappingResource,
+          rdfaNodeType: 'resource',
+          __rdfaId: uuidv4(),
+          properties,
+        };
+      }
+
+      return false;
+    },
+    contentElement: CONTENT_SELECTOR_NEW_MODEL,
   },
 ];
 
