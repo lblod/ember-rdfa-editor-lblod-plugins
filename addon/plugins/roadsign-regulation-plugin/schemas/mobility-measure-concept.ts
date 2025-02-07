410ba41fb5cfbd9f5256c3bc80c160a4ec1b9fdd
Elena Poelman <elena.poelman@redpencil.io>
2025-02-07 12:29:32 +0100

    cleanup unused code

--- addon/plugins/roadsign-regulation-plugin/schemas/mobility-measure-concept.ts
+++ addon/plugins/roadsign-regulation-plugin/schemas/mobility-measure-concept.ts
@@ -1,35 +1,5 @@
-import { tracked } from '@glimmer/tracking';
-import { IBindings } from 'fetch-sparql-endpoint';
-import Sign, { SignConceptSchema } from './sign-concept';
-import { unwrap } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/option';
+import { SignConceptSchema } from './sign-concept';
 import { z } from 'zod';
-
-export default class Measure {
-  @tracked signs: Sign[] = [];
-  classifications = new Set();
-
-  constructor(
-    readonly uri: string,
-    readonly label: string,
-    readonly template: string,
-    readonly preview: string,
-    readonly zonality: string,
-    readonly temporal: string | false,
-    signs: Sign[] = [],
-  ) {
-    this.signs = signs;
-  }
-
-  static fromBinding(binding: IBindings) {
-    const uri = unwrap(binding['uri']?.value);
-    const label = unwrap(binding['label']?.value);
-    const template = unwrap(binding['template']?.value);
-    const temporal = binding['temporal']?.value ?? false;
-    const preview = unwrap(binding['preview']?.value);
-    const zonality = unwrap(binding['zonality']?.value);
-    return new Measure(uri, label, template, preview, zonality, temporal);
-  }
-}
 
 export const MobilityMeasureConceptSchema = z.object({
   uri: z.string(),
--- addon/plugins/roadsign-regulation-plugin/schemas/sign-concept.ts
+++ addon/plugins/roadsign-regulation-plugin/schemas/sign-concept.ts
@@ -1,38 +1,4 @@
-import { unwrap } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/option';
-import { IBindings } from 'fetch-sparql-endpoint';
 import { z } from 'zod';
-
-export default class Sign {
-  constructor(
-    readonly code: string,
-    readonly type: string,
-    readonly image: string,
-    readonly classifications: string[] = [],
-    readonly uri: string,
-    readonly order: string,
-    readonly zonality?: string,
-  ) {}
-  static fromBinding(binding: IBindings) {
-    const code = unwrap(binding['code']?.value);
-    const image = Sign.processImage(
-      unwrap(binding['image']?.value),
-      unwrap(binding['imageBaseUrl']?.value),
-    );
-
-    const type = unwrap(binding['type']?.value);
-    const uri = unwrap(binding['uri']?.value);
-    // const order = unwrap(binding['order']?.value);
-
-    const classifications = binding['classifications']?.value.split('|') ?? [];
-    const zonality = binding['zonality']?.value;
-
-    return new Sign(code, type, image, classifications, uri, '', zonality);
-  }
-
-  static processImage(imageId: string, imageBaseUrl: string) {
-    return `${imageBaseUrl}/files/${imageId}/download`;
-  }
-}
 
 export const SignConceptSchema = z.object({
   uri: z.string(),
