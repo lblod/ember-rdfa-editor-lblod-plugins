cf656170e03a47050a168b5d40978123a79b6bb2
Elena Poelman <elena.poelman@redpencil.io>
2025-02-11 16:37:16 +0100

    variables: clean-up unnecessary autofill-variable parserule

--- addon/plugins/variable-plugin/variables/autofilled.ts
+++ addon/plugins/variable-plugin/variables/autofilled.ts
@@ -1,5 +1,4 @@
 import {
-  DCT,
   EXT,
   RDF,
 } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
@@ -8,29 +7,21 @@ import {
   createEmberNodeView,
   EmberNodeConfig,
 } from '@lblod/ember-rdfa-editor/utils/ember-node';
-import { v4 as uuidv4 } from 'uuid';
 import {
   DOMOutputSpec,
   getRdfaAttrs,
   PNode,
   rdfaAttrSpec,
 } from '@lblod/ember-rdfa-editor';
-import {
-  hasRdfaVariableType,
-  isVariable,
-  parseLabel,
-  parseVariableInstance,
-  parseVariableType,
-} from '@lblod/ember-rdfa-editor-lblod-plugins/utils/variable-attribute-parsers';
+import { hasRdfaVariableType } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/variable-attribute-parsers';
 import NodeViewComponent from '@lblod/ember-rdfa-editor-lblod-plugins/components/variable-plugin/variable/nodeview';
 import type { ComponentLike } from '@glint/template';
 import { hasOutgoingNamedNodeTriple } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/namespace';
 import { renderRdfaAware } from '@lblod/ember-rdfa-editor/core/schema';
-import { sayDataFactory } from '@lblod/ember-rdfa-editor/core/say-data-factory';
 import { recreateVariableUris } from '../utils/recreate-variable-uris';
 
-const CONTENT_SELECTOR = `span[property~='${EXT('content').prefixed}'],
-                          span[property~='${EXT('content').full}']`;
+const CONTENT_SELECTOR = '[data-content-container="true"]';
+
 const rdfaAware = true;
 const parseDOM = [
   {
@@ -42,7 +33,7 @@ const parseDOM = [
       }
       if (
         hasOutgoingNamedNodeTriple(attrs, RDF('type'), EXT('Mapping')) &&
-        node.querySelector('[data-content-container="true"]') &&
+        node.querySelector(CONTENT_SELECTOR) &&
         hasRdfaVariableType(attrs, 'autofilled')
       ) {
         if (attrs.rdfaNodeType !== 'resource') {
@@ -53,65 +44,6 @@ const parseDOM = [
         const initialized = node.dataset.initialized === 'true';
         return {
           ...attrs,
-          autofillKey,
-          convertToString,
-          initialized,
-        };
-      }
-
-      return false;
-    },
-    contentElement: '[data-content-container="true"]',
-  },
-  {
-    tag: 'span',
-    getAttrs: (node: HTMLElement) => {
-      if (
-        isVariable(node) &&
-        node.querySelector(CONTENT_SELECTOR) &&
-        parseVariableType(node) === 'autofilled'
-      ) {
-        const mappingSubject =
-          node.getAttribute('subject') ||
-          node.getAttribute('resource') ||
-          node.getAttribute('about');
-        if (!mappingSubject) {
-          return false;
-        }
-        const variableInstance = parseVariableInstance(node);
-        const label = parseLabel(node);
-        const autofillKey = node.dataset.autofillKey;
-        const convertToString = node.dataset.convertToString === 'true';
-        const initialized = node.dataset.initialized === 'true';
-        return {
-          __rdfaId: uuidv4(),
-          subject: mappingSubject,
-          rdfaNodeType: 'resource',
-          properties: [
-            {
-              predicate: RDF('type').full,
-              object: sayDataFactory.namedNode(EXT('Mapping').full),
-            },
-            {
-              predicate: EXT('instance').full,
-              object: sayDataFactory.namedNode(
-                variableInstance ??
-                  `http://data.lblod.info/variables/${uuidv4()}`,
-              ),
-            },
-            {
-              predicate: EXT('label').full,
-              object: sayDataFactory.literal(label || ''),
-            },
-            {
-              predicate: DCT('type').full,
-              object: sayDataFactory.literal('autofilled'),
-            },
-            {
-              predicate: EXT('content').full,
-              object: sayDataFactory.contentLiteral(),
-            },
-          ],
           autofillKey,
           convertToString,
           initialized,
