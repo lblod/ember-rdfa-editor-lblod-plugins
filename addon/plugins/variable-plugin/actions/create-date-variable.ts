8549a3a4220af81f3adecbc40348318a381059bd
Elena Poelman <elena.poelman@redpencil.io>
2025-03-10 13:36:21 +0100

    feat: convert date variable to smart var system

--- addon/plugins/variable-plugin/actions/create-date-variable.ts
+++ addon/plugins/variable-plugin/actions/create-date-variable.ts
@@ -5,8 +5,13 @@ import {
   VARIABLES,
   XSD,
 } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
-import { OutgoingTriple } from '@lblod/ember-rdfa-editor/core/rdfa-processor';
+import { AllOrNone } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/types';
+import {
+  FullTriple,
+  IncomingLiteralNodeTriple,
+} from '@lblod/ember-rdfa-editor/core/rdfa-processor';
 import { sayDataFactory } from '@lblod/ember-rdfa-editor/core/say-data-factory';
+import { formatContainsTime } from '../utils/date-helpers';
 
 type CreateDateVariableArgs = {
   schema: Schema;
@@ -19,60 +24,62 @@ export function createDateVariable(args: CreateDateVariableArgs) {
 }
 
 type CreateDateVariableAttrsArgs = {
-  variable: string;
-  variableInstance: string;
   label?: string;
   value?: string;
+  /** @deprecated the `onlyDate` attribute is no longer used. It has been superseded by the usage of `format` and `datatype` */
   onlyDate?: boolean;
   format?: string;
   custom?: boolean;
   customAllowed?: boolean;
-};
+} & AllOrNone<{ variable: string; variableInstance: string }>;
 
 export function createDateVariableAttrs({
   variable,
   variableInstance,
   label,
   value,
-  onlyDate,
   format,
   custom,
   customAllowed,
 }: CreateDateVariableAttrsArgs) {
-  const properties: OutgoingTriple[] = [
-    {
-      predicate: RDF('type').full,
-      object: sayDataFactory.namedNode(VARIABLES('VariableInstance').full),
-    },
-    {
-      predicate: VARIABLES('instanceOf').full,
-      object: sayDataFactory.namedNode(variable),
-    },
-    {
-      predicate: DCT('type').full,
-      object: sayDataFactory.literal('date'),
-    },
-  ];
-  if (label) {
-    properties.push({
-      predicate: DCT('title').full,
-      object: sayDataFactory.literal(label),
+  const externalTriples: FullTriple[] = [];
+  const backlinks: IncomingLiteralNodeTriple[] = [];
+  if (variable) {
+    externalTriples.push(
+      {
+        subject: sayDataFactory.namedNode(variableInstance),
+        predicate: RDF('type').full,
+        object: sayDataFactory.namedNode(VARIABLES('VariableInstance').full),
+      },
+      {
+        subject: sayDataFactory.namedNode(variableInstance),
+        predicate: VARIABLES('instanceOf').full,
+        object: sayDataFactory.namedNode(variable),
+      },
+      {
+        subject: sayDataFactory.namedNode(variableInstance),
+        predicate: DCT('type').full,
+        object: sayDataFactory.literal('date'),
+      },
+    );
+    backlinks.push({
+      subject: sayDataFactory.literalNode(variableInstance),
+      predicate: RDF('value').full,
     });
   }
-  if (value) {
-    properties.push({
-      predicate: RDF('value').full,
-      object: sayDataFactory.literal(
-        value,
-        onlyDate ? XSD('date').namedNode : XSD('dateTime').namedNode,
-      ),
-    });
+  let datatype: string | undefined;
+  if (format) {
+    datatype = !formatContainsTime(format)
+      ? XSD('time').full
+      : XSD('dateTime').full;
   }
   return {
-    subject: variableInstance,
-    rdfaNodeType: 'resource',
-    properties,
-    onlyDate,
+    rdfaNodeType: 'literal',
+    datatype: datatype,
+    externalTriples,
+    backlinks,
+    label,
+    content: value,
     format,
     customAllowed,
     custom,
