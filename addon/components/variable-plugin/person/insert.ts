39374aa6213df0495ac7914e4863b385010a1d84
Elena Poelman <elena.poelman@redpencil.io>
2025-03-14 14:51:57 +0100

    feat: convert person variable to smart var system

--- addon/components/variable-plugin/person/insert.ts
+++ addon/components/variable-plugin/person/insert.ts
@@ -3,15 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { type SayController } from '@lblod/ember-rdfa-editor';
-import { sayDataFactory } from '@lblod/ember-rdfa-editor/core/say-data-factory';
-import { v4 as uuidv4 } from 'uuid';
 import IntlService from 'ember-intl/services/intl';
-import {
-  DCT,
-  EXT,
-  RDF,
-} from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
 import { replaceSelectionWithAndSelectNode } from '@lblod/ember-rdfa-editor-lblod-plugins/commands';
+import { createPersonVariable } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin/actions/create-person-variable';
 
 type Args = {
   controller: SayController;
@@ -41,41 +35,14 @@ export default class PersonVariableInsertComponent extends Component<Args> {
 
   @action
   insert() {
-    const mappingSubject = `http://data.lblod.info/mappings/${
-      this.args.templateMode ? '--ref-uuid4-' : ''
-    }${uuidv4()}`;
-    const variableInstance = `http://data.lblod.info/variables/${
-      this.args.templateMode ? '--ref-uuid4-' : ''
-    }${uuidv4()}`;
-    const variableId = uuidv4();
-
     const placeholder = this.intl.t('variable.person.label', {
       locale: this.documentLanguage,
     });
 
     const label = this.label ?? placeholder;
-    const node = this.schema.nodes.person_variable.create({
-      subject: mappingSubject,
-      rdfaNodeType: 'resource',
-      __rdfaId: variableId,
-      properties: [
-        {
-          predicate: RDF('type').full,
-          object: sayDataFactory.namedNode(EXT('Mapping').full),
-        },
-        {
-          predicate: EXT('instance').full,
-          object: sayDataFactory.namedNode(variableInstance),
-        },
-        {
-          predicate: EXT('label').full,
-          object: sayDataFactory.literal(label),
-        },
-        {
-          predicate: DCT('type').full,
-          object: sayDataFactory.literal('person'),
-        },
-      ],
+    const node = createPersonVariable({
+      schema: this.controller.schema,
+      label,
     });
 
     this.label = undefined;
