3b0006783025e0137d65d9587047819159f35f3e
piemonkey <piemonkey@protonmail.ch>
2025-05-27 14:53:39 +0200

    Use on-changed plugin for structures to keep RDFa and numbers up to date

--- addon/plugins/structure-plugin/commands/insert-structure.ts
+++ addon/plugins/structure-plugin/commands/insert-structure.ts
@@ -1,12 +1,9 @@
 import { Command } from '@lblod/ember-rdfa-editor';
-import { transactionCombinator } from '@lblod/ember-rdfa-editor/utils/transaction-utils';
 import {
   type StructurePluginOptions,
   type StructureType,
 } from '../../structure-plugin/structure-types';
-import { recalculateNumbers } from '../monads/recalculate-structure-numbers';
 import { findHowToInsertStructure } from '../monads/insert-structure';
-import { regenerateRdfaLinks } from '../monads/regenerate-rdfa-links';
 import { closeHistory } from '@lblod/ember-rdfa-editor/plugins/history';
 
 export const insertStructure = (
@@ -14,14 +11,13 @@ export const insertStructure = (
   uriGenerator: Required<StructurePluginOptions>['uriGenerator'],
 ): Command => {
   return (state, dispatch) => {
-    const { result, transaction } = transactionCombinator(state)([
-      findHowToInsertStructure(structureType, uriGenerator),
-      // Prevent calculations that will not fail, if the command is just being tested
-      ...(dispatch ? [recalculateNumbers, regenerateRdfaLinks] : []),
-    ]);
+    const { result, transaction } = findHowToInsertStructure(
+      structureType,
+      uriGenerator,
+    )(state, { sayIsDryRun: !dispatch });
 
     transaction.scrollIntoView();
-    if (result.every((ok) => ok)) {
+    if (result) {
       if (dispatch) {
         // makes sure each structure insertion is undoable, even when quickly
         // creating a bunch of them
