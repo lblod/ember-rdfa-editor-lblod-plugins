fa4d251f2056a9a2e4fab701462fe3e1eb3c413d
piemonkey <piemonkey@protonmail.ch>
2025-05-28 16:20:05 +0200

    Prevent weird selection jumping when clicking on snippet nodes

--- addon/plugins/structure-plugin/node.ts
+++ addon/plugins/structure-plugin/node.ts
@@ -138,6 +138,29 @@ export const emberNodeConfig: (
     atom: false,
     editable: rdfaAware,
     tocEntry: buildTocEntry,
+    // This stopEvent would prevent the issue where a cursor jumps temporarily inside a structure
+    // when it is selected (unless it's inside one that is already selected).
+    // Unfortunately, this has the side-effect that if a structure title anywhere in the document
+    // is selected, when any structure name is clicked, the selection remains where it is instead
+    // of moving. Due to the complexity of detecting this situation it seems better to leave the
+    // weird visual bug in place.
+    // stopEvent(event) {
+    //   // Prevent mousedown events from jumping focus inside the ember-node temporarily when
+    //   // selecting by clicking on the title
+    //   if (event.type === 'mousedown') {
+    //     const target = event.target;
+    //     if (
+    //       target instanceof HTMLElement &&
+    //       (target.className.includes('say-structure__header') ||
+    //         target.className.includes('say-structure__name'))
+    //     ) {
+    //       event.preventDefault();
+    //       return true;
+    //     }
+    //   }
+    //   // Follow default stopEvent logic
+    //   return null;
+    // },
     attrs: {
       ...rdfaAttrSpec({ rdfaAware }),
 
