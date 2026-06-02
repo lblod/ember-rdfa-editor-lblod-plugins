f5b8a0948902c9147530b5d0605a386b4ada0671
Kobe Mertens <kobe.mertens@hotmail.com>
2026-06-02 14:34:50 +0200

    move selection forward after location selection

--- addon/plugins/location-plugin/utils/replace-location.ts
+++ addon/plugins/location-plugin/utils/replace-location.ts
@@ -1,12 +1,40 @@
 import { ResolvedPNode } from '@lblod/ember-rdfa-editor/utils/_private/types';
 import { Address } from './address-helpers';
 import { Area, Place } from './geo-helpers';
-import { EditorState, Transaction } from '@lblod/ember-rdfa-editor';
+import {
+  EditorState,
+  Transaction,
+  NodeSelection,
+  TextSelection,
+  PNode,
+  Selection,
+} from '@lblod/ember-rdfa-editor';
 import type { FullTriple } from '@lblod/ember-rdfa-editor/core/rdfa-processor';
 import { PROV } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/constants';
 import { SayDataFactory } from '@lblod/ember-rdfa-editor/core/say-data-factory';
 import { transactionCombinator } from '@lblod/ember-rdfa-editor/utils/transaction-utils';
 import { updateSubject } from '@lblod/ember-rdfa-editor/plugins/rdfa-info/utils';
+
+function moveNodeSelectionForward(selection: Selection, doc: PNode) {
+  if (!(selection instanceof NodeSelection)) {
+    return selection;
+  }
+
+  const $from = selection.$from;
+
+  const index = $from.index();
+  const parent = $from.parent;
+
+  if (index + 1 < parent.childCount) {
+    const nextPos = selection.from + selection.node.nodeSize;
+
+    return NodeSelection.create(doc, nextPos);
+  } else {
+    const posAfter = selection.to;
+
+    return TextSelection.create(doc, posAfter);
+  }
+}
 
 export function replaceLocationCommand(
   node: ResolvedPNode,
@@ -29,22 +57,27 @@ export function replaceLocationCommand(
       }
     }); // update the location value and the external triples
     const tr = state.tr;
-    dispatch(
-      transactionCombinator(
-        state,
-        tr
-          .setNodeAttribute(pos, 'value', location)
-          .setNodeAttribute(pos, 'externalTriples', newExternalTriples),
-      )([
-        // Update the subject uri, while keeping backlinks intact
-        updateSubject({
-          pos,
-          targetSubject: location.uri,
-          keepBacklinks: true,
-          keepProperties: false,
-          keepExternalTriples: true,
-        }),
-      ]).transaction,
+    const setLocationTr = transactionCombinator(
+      state,
+      tr
+        .setNodeAttribute(pos, 'value', location)
+        .setNodeAttribute(pos, 'externalTriples', newExternalTriples),
+    )([
+      // Update the subject uri, while keeping backlinks intact
+      updateSubject({
+        pos,
+        targetSubject: location.uri,
+        keepBacklinks: true,
+        keepProperties: false,
+        keepExternalTriples: true,
+      }),
+    ]).transaction;
+
+    const newSelection = moveNodeSelectionForward(
+      state.selection.map(setLocationTr.doc, setLocationTr.mapping),
+      setLocationTr.doc,
     );
+    setLocationTr.setSelection(newSelection);
+    dispatch(setLocationTr);
   };
 }
