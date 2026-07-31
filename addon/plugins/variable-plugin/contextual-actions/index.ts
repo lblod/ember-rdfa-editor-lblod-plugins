df8c7d8cbf8af96ab30b31aa59d435a904cf8828
piemonkey <piemonkey@protonmail.ch>
2026-07-31 12:49:18 +0200

    Add variable-plugin contextual-actions index for backwards compatibility

---
+++ addon/plugins/variable-plugin/contextual-actions/index.ts
@@ -0,0 +1,6 @@
+export {
+  /** @deprecated these imports have been moved to contextual-actions/place-description.ts */
+  getContextualActions,
+  /** @deprecated these imports have been moved to contextual-actions/place-description.ts */
+  getPlaceDescriptionActionGroups as getContextualActionGroups,
+} from './place-description';
