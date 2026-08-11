00906b2511c827d189d126a3d04415c7a7532382
Kobe Mertens <kobe.mertens@hotmail.com>
2026-08-11 16:12:25 +0200

    fill in replace person command in context options

--- addon/plugins/variable-plugin/contextual-actions/person.ts
+++ addon/plugins/variable-plugin/contextual-actions/person.ts
@@ -4,43 +4,46 @@ import { getRankedPNodes } from '@lblod/ember-rdfa-editor-lblod-plugins/utils/ge
 import { ContextualActionGroup } from '@lblod/ember-rdfa-editor/plugins/contextual-actions';
 import { getPersonFromPNode } from '@lblod/ember-rdfa-editor-lblod-plugins/plugins/variable-plugin/variables/person';
 import { v4 as uuidv4 } from 'uuid';
+import { replacePersonCommand } from '../utils/replace-person';
+import { ResolvedPNode } from '@lblod/ember-rdfa-editor/utils/_private/types';
 
 const PERSON_SUGGESTIONS_GROUP_ID =
   'person-suggestions-9cc92bb6-de78-41cf-824c-ed391b206764';
 
 const SUGGESTION_AMOUNT = 15;
 
-export async function getContextualActions(
-  state: EditorState,
-  searchQuery?: string,
-) {
+export function getContextualActions(state: EditorState, searchQuery?: string) {
   const selectedNode = getSelectedPersonNode(state);
   if (!selectedNode) return [];
   return getRankedPNodes(state, 'person_variable', (node) => node.attrs.subject)
     .map((rankedNode) => rankedNode.node)
-    .filter((node) => selectedNode.attrs.subject !== node.attrs.subject)
+    .filter(
+      (node) => selectedNode.value.attrs.subject !== node.value.attrs.subject,
+    )
     .slice(0, SUGGESTION_AMOUNT)
     .map((node) => {
-      const person = getPersonFromPNode(node);
+      const person = getPersonFromPNode(node.value);
+      if (!person) return;
       return {
         label: person.fullName,
         id: uuidv4(),
         group: PERSON_SUGGESTIONS_GROUP_ID,
-        command: null,
+        command: replacePersonCommand(selectedNode, person),
       };
     })
     .filter(
       (option) =>
-        !searchQuery ||
-        option.label.toLocaleLowerCase().includes(searchQuery.toLowerCase()),
+        option !== undefined &&
+        (!searchQuery ||
+          option.label.toLocaleLowerCase().includes(searchQuery.toLowerCase())),
     );
 }
 
-function getSelectedPersonNode(state: EditorState) {
+function getSelectedPersonNode(state: EditorState): ResolvedPNode | null {
   const { selection } = state;
   return selection instanceof NodeSelection &&
     selection.node.type === selection.node.type.schema.nodes['person_variable']
-    ? selection.node
+    ? { value: selection.node, pos: selection.from }
     : null;
 }
 
